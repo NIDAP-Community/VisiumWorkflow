6bcdc9ce2c1a98bf992758182ee1fd0fecd12db0
Ned Cauley <ned.cauley@nih.gov>
2023-08-30 13:41:39 -0400

    feat (create_visium_object): Added violin plot and spatial feature plot

--- R/create_visium_object.R
+++ R/create_visium_object.R
@@ -1,6 +1,9 @@
 #' Create the initial Seurat ibject from the Visium inputs
 #'
 #' @importFrom Seurat Load10X_Spatial
+#' @importFrom Seurat VlnPlot
+#' @importFrom Seurat SpatialFeaturePlot
+#' @importFrom ggplot theme
 #'
 #' @return
 #' @export
@@ -13,11 +16,21 @@ createVisiumObject <- function(visium.files.directory,
 
   # Load the Visium experiment data as a Seurat object
 
-  Load10X_Spatial(data.dir = visium.files.directory,
+  object <- Load10X_Spatial(data.dir = visium.files.directory,
                   slice = tissue.slice.name,
                   filename = feature.file.name
                   )
 
-  return()
+  # Show the distribution of counts in all spots using a violin plot
+  violin.plot <- VlnPlot(object, features = "nCount_Spatial", pt.size = 0.1) +
+    NoLegend()
+
+  # Show the spatial distribution of counts in each spot on the tissue image
+  spatial.plot <- SpatialFeaturePlot(object, features = "nCount_Spatial") +
+                    theme(legend.position = "right")
+
+  return(list("object" = object,
+              "violin.plot" = violin.plot,
+              "spatial.plot" = spatial.plot))
 
 }
